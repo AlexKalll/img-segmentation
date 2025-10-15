a1363bf3b2368a6307af5558bd81139796f81c7b
AlexKalll <alexkalalw@gmail.com>
2025-10-15 09:31:20 +0300

    chore: Include Windows batch file for easy app launching

---
+++ app_run.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting Image Segmentation Studio (FH Edition)...
+echo.
+echo Make sure you have installed the requirements:
+echo pip install -r requirements.txt
+echo.
+echo Starting Streamlit app...
+streamlit run app/main.py
+pause
+
+
+
